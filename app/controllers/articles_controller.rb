416d8f3c6cdd3ae5533539e25f22a9aea427eddb
Alexander Roberts Dunn <a.rdunn.yw@gmail.com>
2016-07-15 17:13:08 +0100

    updated control

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,6 @@ class ArticlesController < ApplicationController
 	end
 	def create
 		render plain: params[:article].inspect
-		@article = Article.new(article_params)
-		@article.save
-		redirect_to @article
 	end
 	def show
     @article = Article.find(params[:id])
